ec0f1d34f080f881bd1afdb023946bbda87f9191
marcus <andrewmzhang12345@gmail.com>
2016-03-16 18:23:49 -0700

    Added Method to change user roles, working commit

--- client/views/accounts/users.js
+++ client/views/accounts/users.js
@@ -12,6 +12,14 @@ Template.users.helpers({
 
    },
 
+    isCurrentUser: function(a) {
+      if (a === Meteor.userId()) {
+          return true;
+      } else {
+          return false;
+      }
+    },
+
     select: function(a, b) {
         if (a === b) {
             return 'selected'
@@ -19,6 +27,41 @@ Template.users.helpers({
         if (a == null && b === 'user') {
             return 'selected'
         }
+    },
+
+    disableIfSuperAdmin: function(userid) {
+        if (Roles.userIsInRole(userid, 'superAdmin')) {
+            return "disabled";
+        }
+
+        return "";
+    },
+
+    isSuper: function(a) {
+        if (Roles.userIsInRole(a, 'superAdmin')) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+
+});
+
+Template.users.events({
+    'change [name="userRole"]': function(event, template) {
+        var role = $(event.target).find('option:selected').val();
+        console.log("New role: " + role);
+
+        Meteor.call("setRoleOnUser", {
+            user: this._id,
+            role: role
+        }, function(error, response) {
+            if (error) {
+                sAlert.error(error.reason + "<br>" + response);
+            }
+        });
+
     }
+
 
 });
--- collections/users.js
+++ collections/users.js
@@ -1,1 +1,31 @@
+Meteor.methods({
+    setRoleOnUser: function(options) {
+        check( options, {
+            user: String,
+            role: String
+        });
 
+        if (options.role == 'superAdmin') {
+            throw new Meteor.Error("Check your privilege!!", "Only Andy Zhang is eligible " +
+                "to be a super admin");
+        }
+
+        if (options.role == '' || options.role == 'admin'){
+
+        } else {
+            throw new Meteor.Error("Role invalid", "Either pass an empty role or admin. Do " +
+                "not use anything else!!")
+        }
+
+        if (Roles.userIsInRole(options.user, 'superAdmin')) {
+            throw new Meteor.Error("Check your privileges!!", "Don't mess with the person " +
+                "that designed this system :P");
+        }
+
+        try {
+            Roles.setUserRoles(options.user, [options.role]);
+        } catch (exception) {
+            return exception;
+        }
+    }
+});
